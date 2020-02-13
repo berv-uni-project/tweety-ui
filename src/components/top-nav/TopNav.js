a782ca30ea681d0d0ee4f65ddaa82924c2dbd968
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-02-13 17:21:43 +0700

    Change audiance

--- src/components/top-nav/TopNav.js
+++ src/components/top-nav/TopNav.js
@@ -2,7 +2,9 @@ export default {
   name: "top-nav",
   methods: {
     login() {
-      this.$auth.loginWithRedirect();
+      this.$auth.loginWithRedirect({
+        audiance: process.env.VUE_APP_AUDIANCE || window.location.origin
+      });
     },
     logout() {
       this.$auth.logout({
